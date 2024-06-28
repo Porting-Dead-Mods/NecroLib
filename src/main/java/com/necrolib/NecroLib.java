12e09c0962e069e34939139107208c436fd3b5c6
thepigcat <benpospo@gmail.com>
2024-06-28 18:36:17 +0200

    intial work

---
+++ src/main/java/com/necrolib/NecroLib.java
@@ -0,0 +1,13 @@
+package com.necrolib;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(NecroLib.MODID)
+public class NecroLib {
+    public static final String MODID = "necrolib";
+
+    public NecroLib(IEventBus modEventBus) {
+
+    }
+}
